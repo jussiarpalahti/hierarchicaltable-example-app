e0a43869aed75993591e5060e090d13ec417c1d8
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-08-20 14:34:35 +0300

    Table library works with example table

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,11 @@ import {Table} from 'hierarchical-table';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-console.log("argh!")
+import {extable} from './extable'
 
 ReactDOM.render(
-  <div><h1>Testing</h1><Table /></div>,
+  <div>
+      <h1>Test</h1>
+      <Table data={extable} preview={true} />
+  </div>,
   document.getElementById('app'));
