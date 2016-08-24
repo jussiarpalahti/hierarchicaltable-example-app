5221a53ef85f5c0dc8a95498ea43827790be8a1a
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-08-24 23:43:05 +0300

    Add Mobx stuff for state management

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,22 @@ import * as ReactDOM from 'react-dom';
 
 import {extable} from './extable'
 
+import { observable, computed, action } from 'mobx';
+import { observer } from 'mobx-react';
+import DevTools from 'mobx-react-devtools';
+
+
+const App = observer(({ store:{} }) => (
+    <div>
+        <h1>Example app for hierarchical table library</h1>
+        <Table data={extable} preview={true} />
+        <DevTools />
+    </div>
+));
+
+
 ReactDOM.render(
   <div>
-      <h1>Example app for hierarchical table library</h1>
-      <Table data={extable} preview={true} />
+      <App store={{}} />
   </div>,
   document.getElementById('app'));
