65379d93ee3c60cefb111cc395531abf0dc107be
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-08-25 00:42:39 +0300

    Add basic Mobx store

--- src/index.tsx
+++ src/index.tsx
@@ -10,18 +10,40 @@ import { observable, computed, action } from 'mobx';
 import { observer } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 
+class Store {
+
+    @observable table:string;
+    @observable view:string;
+
+    constructor(table:string) {
+        this.table = table;
+        this.view = 'no view';
+    }
+
+    @action set_view(view:string) {
+        this.view = view;
+    }
+
+}
 
 const App = observer(({ store:{} }) => (
+    // <Table data={extable} preview={true} />
     <div>
         <h1>Example app for hierarchical table library</h1>
-        <Table data={extable} preview={true} />
+        <div>
+            Current table and view: "{store.table}" and "{store.view}"
+        </div>
+        <div>Set view:
+            <input onChange={e => store.set_view(e.target.value)} defaultValue={store.view} />
+        </div>
         <DevTools />
     </div>
 ));
 
+const store = new Store('a table');
 
 ReactDOM.render(
   <div>
-      <App store={{}} />
+      <App store={store} />
   </div>,
   document.getElementById('app'));
