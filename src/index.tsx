00f18c225a47f82d2b01afe979114105c12f70e4
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-08-25 12:10:06 +0300

    More generic store hydration to state store

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 
-import {Table} from 'hierarchical-table';
+import {Table, Dataset, ITable} from 'hierarchical-table';
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
@@ -9,6 +9,16 @@ import {extable} from './extable'
 import { observable, computed, action, reaction, toJS} from 'mobx';
 import { observer } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
+
+class DataTable {
+    table:ITable
+}
+
+class DataSource {
+    name:string;
+    url:string;
+    tables:[DataTable]
+}
 
 class StateStore {
 
@@ -60,6 +70,19 @@ class Store {
         this.view = view;
     }
 
+    hydrate(dry_state:any) {
+        // TODO: Make an interface that both Store and dry store could use for data
+        this.table = dry_state.table;
+        this.view = dry_state.view;
+    }
+
+    dehydrate():any {
+        // TODO: Make an interface that both Store and dry store could use for data
+        return {
+            table: toJS(this.table),
+            view: toJS(this.view)}
+    }
+
 }
 
 // <Table data={extable} preview={true} />
@@ -78,7 +101,7 @@ class Store {
                     <input onChange={e => store.set_view(e.target.value)} value={store.view} />
                 </div>
                 <div>
-                    Active state: "{state_store.state}" / {state_store.active_state} / {state_store.states.length}
+                    Active state: {state_store.active_state} / {state_store.states.length}
                     <div>
                         {state_store.is_prev_state() ? "jee" : "noo"}
                         {state_store.is_next_state() ? "jee" : "noo"}
@@ -99,15 +122,15 @@ const store = new Store('a table');
 const state_store = new StateStore();
 
 reaction(
-    () => store.view,
-    (view) => {
-        state_store.add_state(toJS(view));
+    () => store.dehydrate(),
+    (dry_state) => {
+        state_store.add_state(dry_state);
 });
 
 reaction(
     () => state_store.state,
-    (view) => {
-        store.set_view(view);
+    (dry_state) => {
+        store.hydrate(dry_state);
     }
 );
 
